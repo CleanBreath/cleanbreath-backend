a92b5d5eb179235dbb494be4aff06583025307fc
CMU02 <Hyeonjun1968@naver.com>
2024-08-02 12:36:35 +0900

    - REMOVE : 불필요한 파일 삭제

--- src/main/java/cleanbreath/backend/controller/FeedbackController.java
+++ src/main/java/cleanbreath/backend/controller/FeedbackController.java
@@ -19,30 +19,35 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
+    // 전체 피드백 데이터 가져오기
     @GetMapping("/v1/feedback-list")
     public ResponseEntity<List<ResponseListFeedbackDTO>> FeedbackList() {
         List<ResponseListFeedbackDTO> result = feedbackService.findAllFeedback();
         return ResponseEntity.ok(result);
     }
 
+    // 피드백 추가하기
     @PostMapping("/v1/feedback/add")
     public ResponseEntity<ResponseMessage> addFeedback(@RequestBody RequestSaveFeedBackDTO saveDto) {
         ResponseMessage saveFeedback = feedbackService.save(saveDto);
         return ResponseEntity.ok(saveFeedback);
     }
 
+    // 해당 피드백 가져오기
     @GetMapping("/v1/feedback/{id}")
     public ResponseEntity<?> getFeedbackById(@PathVariable("id") Long id, @RequestBody RequestFeedbackAccountDTO accountDTO) {
         Object findFeedback = feedbackService.findFeedback(id, accountDTO);
         return ResponseEntity.ok(findFeedback);
     }
 
+    // 해당 피드백 업데이트
     @PutMapping("/v1/feedback/{id}")
     public ResponseEntity<ResponseMessage> updateFeedback(@PathVariable("id") Long id, @RequestBody RequestUpdateFeedbackDTO updateDTO) {
         ResponseMessage updateMessage = feedbackService.updateFeedBack(id, updateDTO);
         return ResponseEntity.ok(updateMessage);
     }
 
+    // 해당 피드백 삭제
     @DeleteMapping("/v1/feedback/{id}")
     public ResponseEntity<ResponseMessage> deleteFeedback(@PathVariable("id") Long id) {
         ResponseMessage deleteFeedback = feedbackService.deleteFeedback(id);
--- src/main/java/cleanbreath/backend/dto/FeedbackDTO/RequestDeleteUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package cleanbreath.backend.dto.FeedbackDTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class RequestDeleteUpdateDTO {
-}
