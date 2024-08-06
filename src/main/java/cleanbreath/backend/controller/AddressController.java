4688a30c089329eac321e1124a44e5cd0c861b51
CMU02 <Hyeonjun1968@naver.com>
2024-08-06 12:33:58 +0900

    - Comment : 컨트롤러 주석 추가

--- src/main/java/cleanbreath/backend/controller/AddressController.java
+++ src/main/java/cleanbreath/backend/controller/AddressController.java
@@ -14,11 +14,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1")
 public class AddressController {
 
     private final AddressService addressService;
 
-    @GetMapping("/v1/allData")
+    /**
+     * 금연 및 흡연구역의 모든 정보를 반환한다.
+     * @apiNote 'server.bluesky-cleanbreath.com/v1/allData'
+     */
+    @GetMapping("/allData")
     public ResponseEntity<?> getAllAddresses() {
         List<ResponseAllAddressDTO> result = addressService.getAllAddresses();
         LocalDateTime updateAt = LocalDateTime.now();
@@ -26,13 +31,21 @@ public class AddressController {
         return ResponseEntity.ok(new ResponseAllDataUpdateCheckDTO<>(result.size(), updateAt, result));
     }
 
-    @GetMapping("/v1/data")
+    /**
+     * 위도 및 경도 값을 받아서 일치하는 주소를 반환한다.
+     * @apiNote 'server.bluesky-cleanbreath.com/v1/data?lat=37.235634&lng=126.34264645'
+     */
+    @GetMapping("/data")
     public ResponseEntity<ResponseAddressDTO> getAddress(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng) {
         ResponseAddressDTO findAddress = addressService.getAddress(lat, lng);
         return ResponseEntity.ok(findAddress);
     }
 
-    @PostMapping("/v1/updateDate")
+    /**
+     * 날짜 값을 받아 30일 지나면 최신데이터를 다시 반환하고, 지나지 않으면 업데이트 시기가 아니라고 메시지 반환한다.
+     * @apiNote 'server.bluesky-cleanbreath.com/v1/updateDate
+     */
+    @PostMapping("/updateDate")
     public ResponseEntity<?> updateAddress(@RequestBody RequestCheckUpdateAtDTO updateAtDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(addressService.updateAddress(updateAtDTO));
     }
--- src/main/java/cleanbreath/backend/controller/ApartmentController.java
+++ src/main/java/cleanbreath/backend/controller/ApartmentController.java
@@ -18,11 +18,19 @@ import java.util.List;
 public class ApartmentController {
     private final ApartmentService apartmentService;
 
+    /**
+     * 안양시 공동주택 금연구역 지정 현황 및 위치 정보 가져온다.
+     * @apiNote "server.bluesky-cleanbreath.com/v1/apartment
+     */
     @GetMapping("/apartment")
     public ResponseEntity<List<ResponseAllApartmentDTO>> getAllApartments() {
         return ResponseEntity.ok(apartmentService.getAllApartments());
     }
 
+    /**
+     * 지역 검색하여 해당 지역의 공동주택 금연구역 지정 현황 및 위치 정보 가져온다.
+     * @apiNote "server.bluesky-cleanbreath.com/v1/region?r=동안구"
+     */
     @GetMapping("/region")
     public ResponseEntity<List<ResponseRegionApartmentDTO>> getRegionsApartments(@RequestParam("r") String region) {
         return ResponseEntity.ok(apartmentService.getRegionApartments(region));
